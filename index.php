06cd8a108b20a09400f46c6f5c2823b7d22fe3de
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-07-07 22:52:36 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<? php require_once('inc/connection.php'); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Log In - User Management System</title>
+</head>
+<body>
+
+</body>
+</html>
