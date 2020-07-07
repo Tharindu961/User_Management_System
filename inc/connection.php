fe7c219b6acc8a85a66c5ed20ad9b22043923753
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-07-07 22:42:32 +0530

    Connection part done

--- inc/connection.php
+++ inc/connection.php
@@ -7,6 +7,9 @@
 
 	$connection = mysqli_connect('localhost', 'root', '', 'userdb');
 
-	
+	// Checking the connection
+	if (mysqli_connect_errno()) {
+		die('Database connection failed ' . mysqli_connect_errno());
+	}
 
 ?>
