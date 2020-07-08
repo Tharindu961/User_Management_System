dd628af469f00a1280f310408a1a2b48055938f8
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-07-08 08:28:03 +0530

    Creting login form

--- index.php
+++ index.php
@@ -7,7 +7,15 @@
 	<title>Log In - User Management System</title>
 </head>
 <body>
+	<div class="login">
 
+		<form action="index.php" method="post">
+			<fieldset>
+				<legend><h1>Log In</h1></legend>
+			</fieldset>
+		</form>
+
+	</div>
 </body>
 </html>
 <?php mysqli_close($connection); ?>
