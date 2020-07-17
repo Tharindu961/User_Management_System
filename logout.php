4695f654048ce0124a1051f50f0481cb1fefa4ca
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-07-17 21:43:10 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -5,8 +5,12 @@
 	$_SESSION = array();
 
 	if (isset($_COOKIE[session_name()])) {
-		setcookie(session_name(), '', time()}
+		setcookie(session_name(), '', time()-86400, '/'};
 	}
+
+	session_destroy();
+
+	
 
 
 ?>
